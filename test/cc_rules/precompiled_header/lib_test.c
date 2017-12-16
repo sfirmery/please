32c78a8355028b2cc50e43023da5e9f467646d69
Sylvain Firmery <sylvain@firmery.fr>
2017-12-16 14:23:04 +0100

    Add a cc_rule for precompiled headers

---
+++ test/cc_rules/precompiled_header/lib_test.c
@@ -0,0 +1,14 @@
+#include "all_headers.h"
+
+char kTemplate[] = "<testsuite errors=\"0\" failures=\"0\"></testsuite>";
+
+int main(int argc, char** argv) {
+  // The following line is legal C but not legal C++.
+  char* buf = malloc(strlen(kTemplate) * sizeof(char) + 1);
+  strcpy(buf, kTemplate);
+  FILE* f = fopen("test.results", "w");
+  fprintf(f, "%s\n", buf);
+  fclose(f);
+  printf("%d %s\n", argc, argv[0]);
+  return 0;
+}
